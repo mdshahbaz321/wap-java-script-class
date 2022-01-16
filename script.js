80e11cbd4dbd7ec26fcd4e6deb6d0a2417402dea
mdshahbaz321 <97720411+mdshahbaz321@users.noreply.github.com>
2022-01-16 16:54:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function wap()
+{
+    var x = document.getElementById("big-image");
+    x.src = "./images/b.jpg";
+}
+function wap1()
+{
+    var x = document.getElementById("big-image");
+    x.src = "./images/c.jpg";
+} 
+function wap2()
+{
+    var x = document.getElementById("big-image");
+    x.src = "./images/d.jpg";
+}
+function wap3()
+{
+    var x = document.getElementById("big-image");
+    x.src = "./images/e.jpg";
+}
+function wap4()
+{
+    var x = document.getElementById("big-image");
+    x.src = "./images/a.jpg";
+}
