38785c94d2ba2bcd74da39fdc9e22d6a45d959ed
mdshahbaz321 <97720411+mdshahbaz321@users.noreply.github.com>
2022-01-17 11:07:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
+
 function wap()
 {
-    var x = document.getElementById("big-image");
-    x.src = "./images/b.jpg";
+    var x = document.getElementById("text");
+    x.style.color = "red";
 }
 function wap1()
 {
-    var x = document.getElementById("big-image");
-    x.src = "./images/c.jpg";
-} 
+    var x =document.getElementById("text");
+    x.style.fontFamily = "scream real"
+
+}
 function wap2()
 {
-    var x = document.getElementById("big-image");
-    x.src = "./images/d.jpg";
+    var x =document.getElementById("text");
+    x.style.fontSize = "50px"
+
 }
 function wap3()
 {
-    var x = document.getElementById("big-image");
-    x.src = "./images/e.jpg";
+    var x =document.getElementById("text");
+    x.style.border = "10px groove red"
 }
 function wap4()
 {
-    var x = document.getElementById("big-image");
-    x.src = "./images/a.jpg";
+    var x = document.getElementById("result");
+    x.style.background = "blue";
+}
+function wap5()
+{
+    var x = document.getElementById("text");
+    x.style.margin = "50px auto";
+}
+function wap6()
+{
+    var x = document.getElementById("text");
+    x.style.padding = "100px"
+}
+function wap7()
+{
+    var x = document.getElementById("text");
+    x.style.textAlign = "right";
+}
+function wap8()
+{
+    var x = document.getElementById("text");
+    var y = 1+2;
+        x.innerHTML = y
+        
+}
+function wap9()
+{
+    var x = document.getElementById("text");
+    var y = 44-33;
+        x.innerHTML = y
+        
+}
+function wap10()
+{
+    var x = document.getElementById("text");
+    var y = 33*5;
+        x.innerHTML = y
+        
+}
+function wap11()
+{
+    var x = document.getElementById("text");
+    var y = 555/5;
+        x.innerHTML = y
+        
 }
